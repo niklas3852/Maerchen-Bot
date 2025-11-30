9e3baa88e331fd09851ddab05d6193d024fbc3bf
niklas3852 <niklas-ulbrich@web.de>
2025-11-30 22:02:34 +0100

    Delete Advent_im_Maerchenland directory

--- Advent_im_Maerchenland/events/error.js
+++
@@ -1,4 +0,0 @@
-module.exports = (bot, err) => {
-    console.log(bot.locale.error);
-    return console.error(err);
-}
--- Advent_im_Maerchenland/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /opt/bot/Advent_im_Maerchenland
-/usr/bin/node .
